387ee4314887cb04b7b01de5aba8812f5d30cf32
Nicolas Consalvi <151012570+NicoC54@users.noreply.github.com>
2026-09-06 11:48:30 +0200

    Create 27-RemoveElement

---
+++ solutions/27-RemoveElement/27-RemoveElement.cpp
@@ -0,0 +1,19 @@
+// Last updated: 06/09/2026 11:48:29
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        int k = 0;
+
+        for (int i=0; i< nums.size(); i++){
+
+            if (nums[i] != val) {
+                nums[k]= nums[i];
+                k = k +1;
+        }
+    
+    }
+    return k;
+    
+}
+};
