a26d9b7d7ce813b039bc98bb132e79d0188717d0
Nicolas Consalvi <151012570+NicoC54@users.noreply.github.com>
2026-09-15 12:00:14 +0200

    Create 14-LongestCommonPrefix

---
+++ solutions/14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -0,0 +1,41 @@
+// Last updated: 15/09/2026 12:00:14
+#include <map>
+#include <vector>
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        std::map<int,char> Common ={};
+        int counter=0;
+        int min_counter=0;
+        std::string output = "";
+
+        std::string str = strs[0];
+        for (int i=0; i < str.size(); i++){
+                Common[i] = str[i];
+        }
+
+        min_counter = Common.size();
+
+        for (const std::string& str : strs){
+
+            counter = 0;
+
+            for (int i=0; i< str.size(); i++){
+                if (str[i] == Common[i]){
+                counter +=1; }
+                else{
+                    break;
+                }
+            }
+              if (counter < min_counter){
+                min_counter = counter;}
+
+        }
+        for (int i = 0; i<min_counter;i++){
+            output += Common[i];
+        }
+        return output;
+    }
+};
